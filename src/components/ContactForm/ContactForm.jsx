32511c7d29c31bf32a79485e1d25a87c154df7bb
Antifishka <101981622+Antifishka@users.noreply.github.com>
2022-11-13 14:24:07 +0200

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,7 +1,7 @@
 import { nanoid } from 'nanoid';
 import { BoxForm, FieldForm, InputForm, BtnForm, ErrorText } from './ContactForm.styled';
 import PropTypes from 'prop-types';
-import { Formik, ErrorMessage, useFormik } from 'formik';
+import { Formik, ErrorMessage } from 'formik';
 import * as yup from 'yup';
 
 // library yup
@@ -23,34 +23,32 @@ const schema = yup.object().shape({
 });
  
 // Library formik
-const ContactForm  = ({onSubmit}) => {
-    const formik = useFormik({
-        initialValues: {
-            name: '',
-            number: '',
-        },
+const INITIAL_VALUES = {
+    name: '',
+    number: '',
+};
 
-        onSubmit: (values, actions) => {
-            console.log(values);
+const ContactForm =({onSubmit}) => {
+    const handleSubmit = (values, actions) => {
+        console.log(values);
+        
+        const { name, number } = values;
 
-            const { name, number } = values;
+        onSubmit(name, number);
 
-            onSubmit(name, number);  //проп из App
+        const { resetForm } = actions;
 
-            const { resetForm } = actions;
+        resetForm();
+    };
 
-            resetForm(); 
-        },
-    });
-     
     const nameId = nanoid();
     const numberId = nanoid();
 
     return (
         <Formik
-            initialValues={formik.initialValues}
+            initialValues={INITIAL_VALUES}
             validationSchema={schema}
-            onSubmit={formik.handleSubmit} >
+            onSubmit={handleSubmit} >
             <BoxForm>
                 <FieldForm htmlFor={nameId}>Name
                     <InputForm
@@ -60,7 +58,7 @@ const ContactForm  = ({onSubmit}) => {
                     <ErrorMessage
                         name="name"
                         render={message => <ErrorText>{message}</ErrorText>} />
-                    </FieldForm>
+                </FieldForm>
                 <FieldForm htmlFor={numberId}>Number
                     <InputForm
                         type="tel"
@@ -74,61 +72,7 @@ const ContactForm  = ({onSubmit}) => {
             </BoxForm>
         </Formik>  
     );
- };
-
-
-// const INITIAL_VALUES = {
-//     name: '',
-//     number: '',
-// };
-    
-// export class OldContactForm extends Component {
-//     handleSubmit = (values, actions) => {
-//         console.log(values);
-        
-//         const { name, number } = values;
-
-//         this.props.onSubmit(name, number);
-
-//         const { resetForm } = actions;
-
-//         resetForm();
-//     };
-
-//     render() {
-//         const nameId = nanoid();
-//         const numberId = nanoid();
-
-//         return (
-//             <Formik
-//                 initialValues={INITIAL_VALUES}
-//                 validationSchema={schema}
-//                 onSubmit={this.handleSubmit} >
-//                 <BoxForm>
-//                     <FieldForm htmlFor={nameId}>Name
-//                         <InputForm
-//                             type="text"
-//                             name="name"
-//                             id={nameId} /> 
-//                         <ErrorMessage
-//                             name="name"
-//                             render={message => <ErrorText>{message}</ErrorText>} />
-//                     </FieldForm>
-//                     <FieldForm htmlFor={numberId}>Number
-//                         <InputForm
-//                             type="tel"
-//                             name="number"
-//                             id={numberId} />
-//                         <ErrorMessage
-//                             name="number"
-//                             render={message => <ErrorText>{message}</ErrorText>} />
-//                     </FieldForm>    
-//                     <BtnForm type="submit">Add contact</BtnForm>        
-//                 </BoxForm>
-//             </Formik>  
-//         );
-//     };
-// };
+};
     
 export default ContactForm;
 
