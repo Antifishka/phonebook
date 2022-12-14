da0e1acfd610a923c6a360f65f00a8a8fb3c132d
Antifishka <101981622+Antifishka@users.noreply.github.com>
2022-12-14 20:37:55 +0200

    Delete getVisibleContacts.js

--- src/helpers/getVisibleContacts.js
+++
@@ -1,8 +0,0 @@
-export const getVisibleContacts = (contacts, filter) => {
-  const normalizedFilter = filter.toLowerCase();
-
-  return contacts.filter(({ name }) =>
-    name.toLowerCase().includes(normalizedFilter)
-  );
-};
-
