3344490306e86e60bae1bc7d98a4d5fb01579b0e
Antifishka <101981622+Antifishka@users.noreply.github.com>
2023-02-18 11:56:07 +0200

    Update RegisterForm.jsx

--- src/components/RegisterForm /RegisterForm.jsx
+++ src/components/RegisterForm /RegisterForm.jsx
@@ -5,22 +5,22 @@ import { FaUserEdit } from 'react-icons/fa';
 import { MdEmail, MdLock } from 'react-icons/md';
 import { theme } from 'theme';
 import { Box } from "components/Box/Box";
-// import * as yup from 'yup';
+import * as yup from 'yup';
 
-// const basicSchema = yup.object().shape({
-//     name: yup
-//         .string()
-//         .min(2, 'Too Short!')
-//         .max(50, 'Too Long!')
-//         .matches(
-//             /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
-//             "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan")
-//         .required('Name is required'),
-//     email: yup
-//         .string()
-//         .matches()
-//         .required('Email is required'),
-// });
+const basicSchema = yup.object().shape({
+    name: yup
+        .string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .matches(
+            /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
+            "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan")
+        .required('Name is required'),
+    email: yup
+        .string()
+        .matches()
+        .required('Email is required'),
+});
 
 export const RegisterForm = () => {
   const dispatch = useDispatch();
@@ -29,11 +29,12 @@ export const RegisterForm = () => {
     e.preventDefault();
     const form = e.currentTarget;
     dispatch(
+      basicSchema.validate(
       authOperations.register({
         name: form.elements.name.value,
         email: form.elements.email.value,
         password: form.elements.password.value,
-      })
+      }))
     );
     form.reset();
   };
